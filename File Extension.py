6d5d6d77c31ad3c2d56b476d51495e16d3df95d6
JaswantKanna <67366696+JaswantKanna@users.noreply.github.com>
2020-06-24 15:37:18 +0530

    Create File Extension.py

---
+++ File Extension.py
@@ -0,0 +1,8 @@
+File Extension:
+Input:
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : 'python'")
+Output:
+Input the Filename: abc.py
+The extension of the file is : 'python'
