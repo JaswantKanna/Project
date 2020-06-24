a41192511ad83b85092f2e5ab70147d183e64c28
JaswantKanna <67366696+JaswantKanna@users.noreply.github.com>
2020-06-24 15:34:46 +0530

    Create Area.py,Fileextension.py

---
+++ Area.py,Fileextension.py
@@ -0,0 +1,18 @@
+Area of the circle
+Input:
+from math import pi 
+r = float(input("Input the radius of the circle:\t\t"))
+A = pi*r*r
+print("\nThe area of the circle with the radius  " + str(r) + "  is  :\t\t", A)
+Output:
+Input the radius of the circle:		1.1
+The area of the circle with the radius  1.1  is  :		 3.8013271108436504
+
+File Extension:
+Input:
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : 'python'")
+Output:
+Input the Filename: abc.py
+The extension of the file is : 'python'
